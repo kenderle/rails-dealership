9f6ff85755808367299deafb0b9709912351808d
Kristin <kbekk2@gmail.com>
2020-08-22 22:09:50 -0500

    Added route info for employees actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :employees
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      namespace :employees do
+        post :login
+        post :create
+        get :me
+        delete :logout
+      end
+    end
+  end
 end
