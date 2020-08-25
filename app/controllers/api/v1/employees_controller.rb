faab665385c38f1f4955200385146f20227149c2
Kristin <kbekk2@gmail.com>
2020-08-24 19:41:18 -0500

    Updated other name flaws in employees controller

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -3,18 +3,18 @@ class Api::V1::EmployeesController < ApplicationController
     skip_before_action :authenticate, only: [:login, :create]
 
     def login
-        render json: { error: "User not authenticated" }, status: 401 and return unless @user=UsersService.login(params[:username], params[:password])
-        render json: @user.profile, status: :ok
+        render json: { error: "User not authenticated" }, status: 401 and return unless @employee=EmployeesService.login(params[:username], params[:password])
+        render json: @employee.profile, status: :ok
     end
 
     def create
-        @user=UsersService.register(params[:position], params[:first_name], params[:last_name], params[:username], params[:password], params[:password_confirmation])
-        render json: { error: "There was a problem saving your user" }, status: :unprocessable_entity and return unless @user
-        render json: @user.profile, status: :ok
+        @employee = Employee.register(params[:position], params[:first_name], params[:last_name], params[:username], params[:password], params[:password_confirmation])
+        render json: { error: "There was a problem saving your user" }, status: :unprocessable_entity and return unless @employee
+        render json: @employee.profile, status: :ok
     end
 
     def logout
-        render json: { error: "There was a problem logging out" }, status: :unprocessable_entity and return unless UsersService.logout(@current_user)
+        render json: { error: "There was a problem logging out" }, status: :unprocessable_entity and return unless EmployeesService.logout(@current_user)
         render json: { success: "You have been successfully logged out" }, status: :ok
     end
 
@@ -24,8 +24,8 @@ class Api::V1::EmployeesController < ApplicationController
 
     private
 
-    def user_params
-        params.require(:user).permit(:first_name, :last_name, :username, :position, :password, :password_confirmation)
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :username, :position, :password, :password_confirmation)
     end
 end
 
